e755caf66835606d3ea4e8a9c34e3a7900aa2916
Osmar Queiroz <ramsosmar@gmail.com>
2015-09-28 21:12:11 -0300

    ajuste dos ambientes

---
+++ js/estrutura.js
@@ -0,0 +1,21 @@
+var Segmento = function (largura, altura, posicao_x, posicao_y) {
+    this.largura = largura || 10;
+    this.altura = altura || 10;
+    this.posicao_x = posicao_x || 0;
+    this.posicao_y = posicao_y || 0;
+};
+
+var MapaRua = function (imagem) {
+    this.imagem = imagem || null;
+    this.listaSegmentos = [];
+    this.adicionarSegmento = function (id, segmento) {
+        this.listaSegmentos[id] = segmento;
+    }
+}
+var MapaVeiculo = function (imagem) {
+    this.imagem = imagem || null;
+    this.listaSegmentos = [];
+    this.adicionarSegmento = function (id, segmento) {
+        this.listaSegmentos[id] = segmento;
+    }
+}
