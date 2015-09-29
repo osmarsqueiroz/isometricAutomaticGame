95203d412ddd7fdef1d866a0c1f892f287edb89d
Osmar Queiroz <ramsosmar@gmail.com>
2015-09-28 21:11:50 -0300

    ajuste dos ambientes

---
+++ js/mapaCenarioIso.js
@@ -0,0 +1,22 @@
+
+
+var mapaRuaIsometrico = new MapaRua("image/iso_vertical_city.png");
+
+mapaRuaIsometrico.adicionarSegmento("0000", new Segmento(132, 100, 0, 1));
+mapaRuaIsometrico.adicionarSegmento("0100", new Segmento(132, 100, 0, 107));
+mapaRuaIsometrico.adicionarSegmento("0010", new Segmento(132, 100, 0, 213));
+mapaRuaIsometrico.adicionarSegmento("0001", new Segmento(132, 100, 0, 319));
+mapaRuaIsometrico.adicionarSegmento("1000", new Segmento(132, 100, 0, 425));
+
+mapaRuaIsometrico.adicionarSegmento("0101", new Segmento(132, 100, 0, 531));
+mapaRuaIsometrico.adicionarSegmento("1010", new Segmento(132, 100, 0, 637));
+
+mapaRuaIsometrico.adicionarSegmento("1101", new Segmento(132, 100, 0, 743));
+mapaRuaIsometrico.adicionarSegmento("1110", new Segmento(132, 100, 0, 849));
+mapaRuaIsometrico.adicionarSegmento("0111", new Segmento(132, 100, 0, 955));
+mapaRuaIsometrico.adicionarSegmento("1011", new Segmento(132, 100, 0, 1061));
+mapaRuaIsometrico.adicionarSegmento("1111", new Segmento(132, 100, 0, 1167));
+mapaRuaIsometrico.adicionarSegmento("0110", new Segmento(132, 100, 0, 1273));
+mapaRuaIsometrico.adicionarSegmento("0011", new Segmento(132, 100, 0, 1379));
+mapaRuaIsometrico.adicionarSegmento("1100", new Segmento(132, 100, 0, 1485));
+mapaRuaIsometrico.adicionarSegmento("1001", new Segmento(132, 100, 0, 1591));
